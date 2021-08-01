f7a2bf64916936d736c613ec3fed9443ab5d2778
Ivan Bond <ivanboarthrills@gmail.com>
2021-08-02 04:42:46 +0700

    fixed preloader, mobile-menu

--- app/js/main.js
+++ app/js/main.js
@@ -3,14 +3,16 @@
 //WOW
 new WOW().init();
 
-
+window.addEventListener('scroll', function() {
+  iconMenu.classList.remove('_active');
+  menuBody.classList.remove('_active');
+});
 //menu burger
 
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
 if (iconMenu){
   iconMenu.addEventListener('click', function() {
-    document.body.classList.toggle('_lock');
     iconMenu.classList.toggle('_active');
     menuBody.classList.toggle('_active');
   });
@@ -35,7 +37,6 @@ if (menuLinks.length > 0) {
       const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
       
       if(iconMenu.classList.contains('_active')) {
-        document.body.classList.remove('_lock');
         iconMenu.classList.remove('_active');
         menuBody.classList.remove('_active');
       }
