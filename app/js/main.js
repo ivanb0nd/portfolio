6be9cc46fd858b10e125627cfa02b76f73feef45
Ivan Bond <ivanboarthrills@gmail.com>
2021-08-01 20:20:49 +0700

    added files

---
+++ app/js/main.js
@@ -0,0 +1,67 @@
+'use strict';
+
+//WOW
+new WOW().init();
+
+
+//menu burger
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu){
+  iconMenu.addEventListener('click', function() {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  });
+}
+
+
+// scroll
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuLinks => {
+    menuLinks.addEventListener('click', onMenuLinkClick);
+  });
+
+  // let onMenuLinkClick = function(e) {
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    
+    if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+      
+      if(iconMenu.classList.contains('_active')) {
+        document.body.classList.remove('_lock');
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active');
+      }
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: 'smooth'
+      });
+      e.preventDefault();
+      
+    }
+  }
+  
+}
+
+
+//preloader
+
+let preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', () => {
+  preloader.classList.add('_hide');
+  setTimeout(() => {
+    preloader.remove();
+    if (document.body.classList.contains('_lock')) {
+      document.body.removeAttribute('class');
+    }
+  }, 800);
+});
